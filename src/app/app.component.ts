9ffb10863ea0cda5c48d6c002c71ebb7ca49451d
school-coder <naga4042@gmail.com>
2022-10-31 19:56:21 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import {loadStripe, Stripe, StripeCardElement, StripeElementsOptions} from '@stripe/stripe-js';
+import {loadStripe, Stripe, StripeCardElement, StripeElements, StripeElementsOptions, StripePaymentElement} from '@stripe/stripe-js';
 
 @Component({
   selector: 'my-app',
@@ -10,7 +10,8 @@ export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
 
   stripe: Stripe;
-  card: StripeCardElement;
+  paymentElement: StripePaymentElement;
+  elements: StripeElements;
 
   ngOnInit() {
     this.initStripe();
@@ -22,21 +23,22 @@ export class AppComponent implements OnInit {
     const options: StripeElementsOptions = {
       clientSecret: 'seti_1LyyTjEtCa3jKbc6Z6XBGVrT_secret_MiPI8VhWW8TPikJszsZEtsiSgZ0N6We'
     }
-    const elements = this.stripe.elements();
-    this.card = elements.create('card', { style: {
-      base: {
-    color: "blue",
-    padding: "5px"
-  }
-    }});
-    this.card.mount("#payment-card");
+    this.elements = this.stripe.elements(options);
+    this.paymentElement = this.elements.create('payment');
+    this.paymentElement.mount("#payment-card");
   }
 
   createToken() {
-    this.stripe.createToken(this.card).then( (result) => {
-      console.log(JSON.stringify(result))
-    });
+    // this.stripe.createToken(this.card).then( (result) => {
+    //   console.log(JSON.stringify(result))
+    // });
 
-    this.stripe.setupIn
+    this.stripe.confirmSetup({
+      elements: this.elements,
+      confirmParams: {
+        return_url: 'https://google.co.in'
+      }
+    })
+
   }
 }
