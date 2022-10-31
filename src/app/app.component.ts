9a44d4539a2b81d6291b328b4e077fd171f0dec5
school-coder <naga4042@gmail.com>
2022-10-31 19:40:17 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import {loadStripe, Stripe, StripeCardElement} from '@stripe/stripe-js';
+import {loadStripe, Stripe, StripeCardElement, StripeElementsOptions} from '@stripe/stripe-js';
 
 @Component({
   selector: 'my-app',
@@ -18,6 +18,10 @@ export class AppComponent implements OnInit {
 
   private async initStripe(): Promise<any> {
     this.stripe = await loadStripe('pk_test_ZOqskYHJqUv4QXp1pMubKHd8');
+
+    const options: StripeElementsOptions = {
+      clientSecret: 'seti_1LyyTjEtCa3jKbc6Z6XBGVrT_secret_MiPI8VhWW8TPikJszsZEtsiSgZ0N6We'
+    }
     const elements = this.stripe.elements();
     this.card = elements.create('card', { style: {
       base: {
@@ -31,6 +35,8 @@ export class AppComponent implements OnInit {
   createToken() {
     this.stripe.createToken(this.card).then( (result) => {
       console.log(JSON.stringify(result))
-    })
+    });
+
+    this.stripe.setupIn
   }
 }
